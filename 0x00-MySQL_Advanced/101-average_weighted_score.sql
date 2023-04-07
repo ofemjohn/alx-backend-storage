c44ad6885f6f82b4f29a6465eb480a7615418c09
ofemjohn <ofemjohn@gmail.com>
2023-04-07 01:30:07 +0100

    Write a SQL script that creates a stored procedure

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,16 +1,19 @@
 -- Write a SQL script that creates a stored procedure.
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
-
+DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    UPDATE users AS U
-    JOIN (
-        SELECT U.id, SUM(C.score * C.weight) / SUM(C.weight) AS w_avg 
+    UPDATE users AS U, 
+        (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg 
         FROM users AS U 
-        JOIN corrections AS C ON U.id=C.user_id 
+        JOIN corrections as C ON U.id=C.user_id 
         JOIN projects AS P ON C.project_id=P.id 
-        GROUP BY U.id
-    ) AS WA ON U.id = WA.id
-    SET U.average_score = WA.w_avg;
-END;
+        GROUP BY U.id)
+    AS WA
+    SET U.average_score = WA.w_avg 
+    WHERE U.id=WA.id;
+END
+$$
+DELIMITER ;
+
 
