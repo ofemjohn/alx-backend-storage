ba912aa5f3900ac93cbd9d8a081da1af7f3f2164
ofemjohn <ofemjohn@gmail.com>
2023-04-09 22:59:39 +0100

    listing aall values of a field

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- Write a SQL script that lists all bands with Glam,
+-- rock as their main style, ranked by their longevity.
+SELECT band_name, (2020(split) - 0(formed)) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
