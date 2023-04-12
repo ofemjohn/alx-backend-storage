f980eefaed2267a8d44326581607340292e657eb
ofemjohn <ofemjohn@gmail.com>
2023-04-12 01:01:11 +0100

    function returns a list of collections

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Python function that returns the 
+list of school having a specific topic:
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''returns a list '''
+    documents = mongo_collection.find({"topics": topic})
+    return list(documents)
