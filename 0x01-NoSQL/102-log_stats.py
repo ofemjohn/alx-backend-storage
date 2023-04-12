085ba6b297f3ad741d0a6e341d1e9588b843f17d
ofemjohn <ofemjohn@gmail.com>
2023-04-12 01:11:53 +0100

    sort by ips

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+'''
+adding the top 10 of the most present
+IPs in the collection nginx of the database logs
+'''
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    """ Provides some stats about Nginx logs stored in MongoDB """
+    '''stats about Nginx logs stored in MongoDB '''
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
